fdca9e84c5d3479c04025662cfc2c84c0a276749
adityarao2005 <82911340+adityarao2005@users.noreply.github.com>
2023-06-09 21:18:56 -0400

    Delete Calendar directory

--- Calendar/src/main/java/model/Analogous.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface Analogous<T extends Analogous<T>> {
-
-	boolean identical(T other);
-}
--- Calendar/src/main/java/model/occurrence/DailyOccurrence.java
+++
@@ -1,6 +0,0 @@
-package model.occurrence;
-
-public class DailyOccurrence extends Occurrence {
-	private static final long serialVersionUID = 1L;
-
-}
